c81d4c2fe295ffe4d1f9142d61165bed819c1832
Jon Holmes <holmjona@isu.edu>
2019-08-22 10:49:40 -0600

    Added some things to read from the file that I also added. Added a couple of buttons.

--- MechanicsBase/MainWindow.xaml.cs
+++ MechanicsBase/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,13 +14,52 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MechanicsBase {
+namespace MechanicsBase
+{
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
             InitializeComponent();
+        }
+
+        private void BtnShow_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello!");
+        }
+
+        private void BtnReadFromFile_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // dangerous code goes here. 
+                string[] fileLines = File.ReadAllLines
+                    ("../../data/toc.txt");
+                //MessageBox.Show("It worked");
+
+                //tbOutput.Text = fileLines[8];
+
+                for (int index = 0; index < fileLines.Length; index++)
+                {
+                    tbOutput.Text = tbOutput.Text + fileLines[index];
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                // what happens when it fails goes here. 
+                MessageBox.Show("It did not work");
+
+            }
+
+
+
+
+
         }
     }
 }
